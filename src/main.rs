5aef12fd932a92f4905a386f68a06545cc073994
Ioan Andrasi <andrasi.ioan90@gmail.com>
2025-10-29 20:37:29 +0200

    Create dummy axum server

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,22 @@
+use axum::http::StatusCode;
+use axum::Router;
+use axum::routing::get;
 
 mod config;
 
 #[tokio::main]
 async fn main() -> Result<(), sqlx::Error>{
     config::init_db().await?;
+
+    //Start a server
+    let app: Router = Router::new()
+        .route("/health", get(health));
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:8080").await?;
+    axum::serve(listener, app.into_make_service()).await?;
+    println!("Server is running on 127.0.0.1:8080");
     Ok(())
+}
+
+async fn health() -> StatusCode {
+    StatusCode::OK
 }
